9cc7500b37fe1dd9addaffffe57bcdb1e718bc76
Tyler Fink <tsfink182@stevenscollege.edu>
2019-09-14 06:45:35 -0400

    No Snow - fixed pokedex it is now in order - took away pokemon that were not evolutions of first generation pokemon

---
+++ data/wildPokemon/route1.asm
@@ -0,0 +1,16 @@
+Route1Mons:
+; Grass Mons
+	db $19
+	db 3,PIDGEY
+	db 3,RATTATA
+	db 3,RATTATA
+	db 4,MAGBY
+	db 5,EEVEE
+	db 3,PIDGEY
+	db 3,PIDGEY
+	db 4,ELEKID
+	db 4,PIDGEY
+	db 5,RATTATA
+
+; Water Mons
+	db $00
